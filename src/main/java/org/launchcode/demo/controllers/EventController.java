627eca8ae41a6add89d7cc12dd7dcd8e16b2e226
amesp73 <amitrpatel502@gmail.com>
2023-09-29 15:20:28 -0500

    create and render a form complete

--- src/main/java/org/launchcode/demo/controllers/EventController.java
+++ src/main/java/org/launchcode/demo/controllers/EventController.java
@@ -3,24 +3,24 @@ package org.launchcode.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    @GetMapping
-    public String getAllEvents(Model model) {
-        ArrayList<String> events = new ArrayList<>();
-        events.add("Birthdays");
-        events.add("Christmas");
-        events.add("Thanksgiving");
-        model.addAttribute("events", events);
+    private static List<String> events = new ArrayList<>();
 
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", events);
         return "events/index";
     }
 
@@ -28,5 +28,12 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 }
